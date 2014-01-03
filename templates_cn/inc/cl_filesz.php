8dfa79038d568ec9eca52f706af18c68d86e8bcb
kevin <511639938@qq.com>
2014-01-03 22:07:23 +0800

    重庆时时彩，两面长龙

--- templates_cn/inc/cl_filesz.php
+++ templates_cn/inc/cl_filesz.php
@@ -7,8 +7,9 @@
   Date:2011-12-9
 */
 
-echo '<div class="popsz" stype="display:none">
+echo '<div class="pops" stype="display:block"><table class="wqs" style="float:auto; width:134px;" border="0" cellpadding="0" cellspacing="0" id="cl"><tr class="t_list_caption"><td colspan="2">两面长龙排行</td></tr><tr height="20"><td class="caption_1">第4球 - 合数单</td><td class="red">4期</td></tr><tr height="20"><td class="caption_1">第4球 - 合数单</td><td class="red">4期</td></tr><tr height="20"><td class="caption_1">第4球 - 合数单</td><td class="red">4期</td></tr></table></div>';
+/*echo '<div class="pops" stype="display:block">
 <table bgcolor="#E9BA84" border="0" cellpadding="0" cellspacing="1" width="160" id="cl"></table>
-</div>';
+</div>';*/
 
 ?>
